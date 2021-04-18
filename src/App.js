08e13a2e9fb854080d80a8be07c415ba076aaf15
shaoukii <sallemishaouki53@gmail.com>
2021-04-18 15:07:41 +0100

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './style.css';
+import Img from "./informatique.jpg"
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div style={{ border: 'solid 1px black', maxWidth: '100vw' }}>
+
+        <h1 className="title red">Shaouki sallemi </h1>
+        
+
+<br />
+<img src={Img} alt="dev"/>
+<br />
+<img src="/dev.jpg" alt="myimage" />
+
+<video width='320' height='240' >
+<source src="TOYOTA - Nouvelle YARIS[Mpgun.com].mp4" type="video/mp4"  />
+</video> 
+
+
+</div>
+
+
+     
+
+
+
+
+
+
     </div>
   );
 }
